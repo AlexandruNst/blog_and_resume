c2c3a8113ff4b264159c7546c135f7ceb1d07cbb
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-06-05 20:05:15 +0100

    Added article detail func to featured page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('articles', views.all_articles_list, name='all_articles_list')
+    path('articles', views.all_articles_list, name='all_articles_list'),
+    path('article/<int:pk>/', views.article_detail, name='article_detail'),
 ]
