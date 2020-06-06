f51c514d7908c3d521a464ac35f2e68629f54c20
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-06-06 15:43:22 +0100

    Added basic functionality for about page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('articles', views.all_articles_list, name='all_articles_list'),
     path('article/<int:pk>/', views.article_detail, name='article_detail'),
+    path('about', views.about, name='about'),
 ]
