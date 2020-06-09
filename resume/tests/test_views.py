d9fd6155377eb1940335f8775a0a3974ad34aa39
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-06-10 00:12:43 +0100

    Added basic unit tests for templates used

--- resume/tests/test_views.py
+++ resume/tests/test_views.py
@@ -1,6 +1,26 @@
 from django.test import TestCase
+from blog.models import Article
+from django.contrib.auth.models import User
+from .base import UnitTest
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class TemplateTest(UnitTest):
+    def test_home_page_returns_correct_template(self):
+        self.set_up()
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'blog/featured.html')
+
+    def test_all_articles_page_returns_correct_template(self):
+        self.set_up()
+        response = self.client.get('/articles')
+        self.assertTemplateUsed(response, 'blog/all_articles.html')
+
+    def test_article_detail_page_returns_correct_template(self):
+        self.set_up()
+        response = self.client.get('/article/1/')
+        self.assertTemplateUsed(response, 'blog/article_detail.html')
+
+    def test_resume_page_return_correct_template(self):
+        self.set_up()
+        response = self.client.get('/resume/')
+        self.assertTemplateUsed(response, 'resume/resume.html')
