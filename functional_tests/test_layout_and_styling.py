570541a9855b1ce6a6f78e7860c727f864522080
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-06-10 22:19:40 +0100

    Added functional tests for styles on resume page

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -6,6 +6,8 @@ class LayoutAndStylingTest(FunctionalTest):
         #Ellie goes to the home page
         self.browser.get('http://127.0.0.1:8000')
         self.browser.set_window_size(1024, 768)
+        self.wait_for(
+            lambda: self.browser.find_elements_by_css_selector('.container'))
 
         #She notices the articles are nicely centered
         article_container = self.browser.find_element_by_css_selector(
@@ -19,6 +21,8 @@ class LayoutAndStylingTest(FunctionalTest):
         #Ellie goes to the article page
         self.browser.get('http://127.0.0.1:8000/articles')
         self.browser.set_window_size(1024, 768)
+        self.wait_for(
+            lambda: self.browser.find_elements_by_css_selector('.container'))
 
         #She notices the articles are nicely centered
         article_container = self.browser.find_element_by_css_selector(
@@ -29,13 +33,30 @@ class LayoutAndStylingTest(FunctionalTest):
                                delta=10)
 
     def test_layout_and_styling_about(self):
-        #Ellie goes to the article page
+        #Ellie goes to the about page
         self.browser.get('http://127.0.0.1:8000/about')
         self.browser.set_window_size(1024, 768)
+        self.wait_for(
+            lambda: self.browser.find_elements_by_css_selector('.container'))
 
         #She notices the content nicely centered
         article_container = self.browser.find_element_by_css_selector(
             '.-about-container')
+        self.assertAlmostEqual(article_container.location['x'] +
+                               article_container.size['width'] / 2,
+                               512,
+                               delta=10)
+
+    def test_layout_and_styling_resume(self):
+        #Ellie goes to the resume page
+        self.browser.get('http://127.0.0.1:8000/resume/')
+        self.browser.set_window_size(1024, 768)
+        self.wait_for(
+            lambda: self.browser.find_elements_by_css_selector('.container'))
+
+        #She notices the resume nicely centered
+        article_container = self.browser.find_element_by_css_selector(
+            '.-resume-container')
         self.assertAlmostEqual(article_container.location['x'] +
                                article_container.size['width'] / 2,
                                512,
