e0ad9cf9cc7773b9aaea1287ac3e9b077d4ac31e
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-06-10 17:34:46 +0100

    ArticleModel - added test for get_list_of_tags function

--- resume/tests/test_models.py
+++ resume/tests/test_models.py
@@ -76,14 +76,22 @@ class ArticleModelTest(UnitTest):
         self.assertEqual(list(Article.objects.all()),
                          [article1, article2, article3])
 
-    def test_default_time(self):
+    def create_dummy_article(self):
         user = User.objects.create_user('test_user', 'test@user.com',
                                         'mostsecurepasswordever')
-        article = Article.objects.create(author=user,
-                                         title='title',
-                                         text='text',
-                                         description='description',
-                                         tags='tags')
+        return Article.objects.create(author=user,
+                                      title='title',
+                                      text='text',
+                                      description='description',
+                                      tags='tag1 tag2 tag3')
+
+    def test_default_time(self):
+        article = self.create_dummy_article()
         self.assertAlmostEqual(article.created_date,
                                timezone.now(),
                                delta=timedelta(seconds=2))
+
+    def test_get_list_of_tags(self):
+        article = self.create_dummy_article()
+        self.assertEqual(article.get_list_of_tags(),
+                         ['#tag1', '#tag2', '#tag3'])
