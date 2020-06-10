c199f7e064709421911c7f8d282962d2867074ab
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-06-10 01:16:54 +0100

    Created ResumeItem model

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ResumeItem(models.Model):
+    SECTIONS = (
+        ('SK', 'Skills'),
+        ('EX', 'Experience'),
+        ('ED', 'Education'),
+        ('TI', 'Technical Interests'),
+    )
+    section = models.CharField(max_length=2, choices=SECTIONS)
+    title = models.CharField(max_length=200)
+    timeframe = models.CharField(max_length=200, blank=True)
+    text = models.TextField(blank=True)
