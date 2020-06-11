3c9e0d83861fdf4b679a4b618d18b960b1167713
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-06-11 01:10:50 +0100

    Added functional test for logged out user on all articles page

--- functional_tests/test_resume_logged_out_user.py
+++ functional_tests/test_resume_logged_out_user.py
@@ -74,3 +74,46 @@ class LoggedOutUserTest(FunctionalTest):
         self.can_see_on_page('Log in')
 
         # She realises there is no easy way to hack into her friend's app
+
+    def test_all_articles(self):
+        # Ellie goes to the all articles page
+        self.browser.get('http://127.0.0.1:8000/articles')
+        self.wait_for(
+            lambda: self.browser.find_elements_by_css_selector('.container'))
+
+        # She receives all existing articles
+        self.can_see_on_page('-article-container')
+
+        # She doesn't seem to find any way to modify the articles,
+        # which makes sense, since she is not an authorised user of the site
+        # with an active account
+        self.cannot_see_on_page('-edit-link')
+
+        # Since Ellie knows a bit about how her friend's site might work,
+        # she decides to try changing the URL to access that she should
+        # have an account in order to access
+        self.browser.get('http://127.0.0.1:8000/article/new')
+        self.wait_for(
+            lambda: self.browser.find_elements_by_css_selector('.container'))
+
+        # She is immediately stopped by the log in screen
+        self.can_see_on_page('Log in')
+
+        # She decides to try another possible URL that might give her
+        # unauthorised access
+        self.browser.get('http://127.0.0.1:8000/article/1/edit')
+        self.wait_for(
+            lambda: self.browser.find_elements_by_css_selector('.container'))
+
+        # She is immediately stopped by the log in screen again
+        self.can_see_on_page('Log in')
+
+        # She decides to try one last time
+        self.browser.get('http://127.0.0.1:8000/article/1/delete')
+        self.wait_for(
+            lambda: self.browser.find_elements_by_css_selector('.container'))
+
+        # She is immediately stopped by the log in screen again
+        self.can_see_on_page('Log in')
+
+        # She realises there is no easy way to hack into her friend's app
