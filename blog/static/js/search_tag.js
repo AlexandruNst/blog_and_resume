f223b53c008e3158836cc2c9f0ecc10c785031f4
Alexandru Nastase <axn778@cs.bham.ac.uk>
2020-06-24 17:27:40 +0100

    Added click functionality to seach icon

--- blog/static/js/search_tag.js
+++ blog/static/js/search_tag.js
@@ -1,15 +1,9 @@
-// function searchTag(tag) {
-//   $("#tag-input").val(tag);
-//   $("#tag-form").submit(function (e) {
-//     e.preventDefault();
-//   });
-//   return false;
-// }
+$(".input-group-append").click(function (e) {
+  $("#nav-form").submit();
+});
 
 $(".-tag").click(function (e) {
-  console.log("hereeee");
   e.preventDefault();
-  console.log($(this).text());
   $("#tag-input").val($(this).text().substring(1));
   $("#tag-form").submit();
 });
